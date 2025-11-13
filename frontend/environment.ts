71bebb13cbc3db1f08a72c219ee500834173154c
sami51100 <samidriouche@yahoo.fr>
2025-11-13 10:13:38 +0100

    Adding a new nginx server for the frontend container

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://10.0.103.89:8080',
-  webSocketUrl: 'ws://10.0.103.89:8080/gameWs'
+    apiUrl: 'http://10.0.103.89:8080',
+    webSocketUrl: 'ws://10.0.103.89:8080/gameWs'
 };
